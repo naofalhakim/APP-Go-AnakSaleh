97191cfa11cb58ed1f883319cdb80310cb2ae98c
naofalhakim <hakimnaofal@gmail.com>
2024-06-12 08:42:50 +0700

    update: update splash screen

--- src/screens/SplashScreen/index.js
+++ src/screens/SplashScreen/index.js
@@ -1,6 +1,5 @@
 import React, {Component} from 'react';
 import {View} from 'react-native';
-import SplashScreen from 'react-native-splash-screen'
 
 class SplashPage extends Component {
   constructor(props) {
@@ -9,26 +8,6 @@ class SplashPage extends Component {
 
   render() {
     return <View />
-    // return (
-    //   <SafeAreaView
-    //     style={{
-    //       backgroundColor: COLOR.BLUE_PRIMER,
-    //       display: 'flex',
-    //       flex: 1,
-    //       justifyContent: 'center',
-    //       alignItems: 'center',
-    //     }}>
-    //     <StatusBar
-    //       barStyle={'light-content'}
-    //       backgroundColor={COLOR.BLUE_PRIMER}
-    //     />
-    //     <Image source={IMG.bgSplash} style={{width: 'auto', height: 'auto'}} />
-    //     <Image
-    //       source={ICON.ic_splash}
-    //       width={horizontalScale(60)}
-    //       height={verticalScale(20)}
-    //     />
-    //   </SafeAreaView>
     // );
   }
 }

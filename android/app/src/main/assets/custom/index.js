b97f229098c39a21ef2adb77ebee3a4336088ce0
naofalhakim <hakimnaofal@gmail.com>
2024-06-18 04:27:21 +0700

    feat: add fonts and configuration

---
+++ android/app/src/main/assets/custom/index.js
@@ -0,0 +1,14 @@
+// this class provides font of this aplication
+const Font = {
+    NunitoRegular: 'Nunito-Regular',
+    NunitoMedium: 'Nunito-Medium',
+    NunitoLight: 'Nunito-Light',
+    NunitoExtraLight: 'Nunito-ExtraLight',
+    NunitoSemiBold: 'Nunito-SemiBold',
+    NunitoBold: 'Nunito-Bold',
+    NunitoExtraBold: 'Nunito-ExtraBold',
+    NunitoBlack: 'Nunito-Black',
+  };
+  
+  export default Font;
+  

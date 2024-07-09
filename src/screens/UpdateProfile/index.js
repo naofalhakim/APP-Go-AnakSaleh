da7cc8b8dbe3fc81e62ce41cd9978fb8443a782f
naofalhakim <hakimnaofal@gmail.com>
2024-07-10 05:28:26 +0700

    feat: integrate update profile with API

--- src/screens/UpdateProfile/index.js
+++ src/screens/UpdateProfile/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Image, ScrollView, Text, TouchableOpacity, View } from 'react-native';
+import { Image, ScrollView, Text, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import COLOR from '../../utils/ColorSystem';
 import styles from './styles';
@@ -8,11 +8,12 @@ import { horizontalScale, verticalScale } from '../../utils/Metric';
 import HeaderBasic from '../../components/HeaderBasic';
 import { SCREEN_NAME, STORAGE_KEY } from '../../utils/Enum';
 import ICON from '../../assets/icons';
-import PointBar from '../../components/PointBar';
-import { getData, removeData } from '../../services/LocalStorage';
-import { getGenderText } from '../../utils/utils';
+import { getData, storeData } from '../../services/LocalStorage';
 import InputTextBasic from '../../components/InputTextBasic';
 import InputPickerBasic from '../../components/InputPickerBasic';
+import LoaderBasic from '../../components/LoaderBasic';
+import { API_URL } from '../../utils/Urlconfig';
+import axios from 'axios';
 
 class UpdateProfile extends Component {
   formValues = {
@@ -41,6 +42,7 @@ class UpdateProfile extends Component {
     super(props);
     this.state = {
       user: {},
+      isLoading: false,
     }
 
     this._inputValidation = this._inputValidation.bind(this);
@@ -59,9 +61,56 @@ class UpdateProfile extends Component {
     this.setState({ user })
   }
 
+  async setUserData(data){
+    let dataUser = {
+      ...this.state.user,
+      name: data.name,
+      gender: data.gender,
+      age: data.age,
+      phone: data.phone
+    }
+    storeData(STORAGE_KEY.USER_LOGIN, dataUser);
+  }
+
   _doSubmit() {
-    console.log('Login');
-    this.props.navigation.navigate(SCREEN_NAME.LOGIN);
+    this.setState({
+      isLoading: true
+    })
+    let data = {
+      email: this.state.user.email,
+      name: this.formValues.name.value || this.state.user.name,
+      gender: this.formValues.gender.value || this.state.user.gender,
+      age: this.formValues.age.value || this.state.user.age,
+      phone: this.formValues.telp.value || this.state.user.phone
+    };
+
+    let config = {
+      method: 'put',
+      url: API_URL.UPDATE_PROFILE,
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      data: data
+    };
+
+    console.log('data', data)
+
+    axios.request(config)
+      .then((response) => {
+        console.log(JSON.stringify(response.data));
+        this.setState({
+          isLoading: false
+        }, () => {
+          this.setUserData(data)
+          this.props.navigation.navigate(SCREEN_NAME.LOGIN)
+        })
+      })
+      .catch((error) => {
+        console.log(error);
+        this.setState({
+          isLoading: false
+        })
+      });
   }
 
   _inputValidation(id, value, validationStatus, errorMessage) {
@@ -72,13 +121,13 @@ class UpdateProfile extends Component {
     }
 
     if (validationStatus) {
-      let allValid = true;
-      for (let index in this.formValues) {
-        if (!this.formValues[index].validationStatus) {
-          allValid = false;
-          break;
-        }
-      }
+      // let allValid = true;
+      // for (let index in this.formValues) {
+      //   if (!this.formValues[index].validationStatus) {
+      //     allValid = false;
+      //     break;
+      //   }
+      // }
 
       this.setState({ isButtonActive: allValid })
     } else {
@@ -88,12 +137,13 @@ class UpdateProfile extends Component {
 
   render() {
     const { email } = this.state.user
+    console.log(this.formValues,'this.formValues');
     return (
       <SafeAreaView style={styles.container}>
         <HeaderBasic navigation={this.props.navigation} title={'Ubah Profil'} />
 
         <ScrollView style={styles.margin}>
-
+          <LoaderBasic isShow={this.state.isLoading} />
           <View style={styles.photoContainer}>
             <Image source={ICON.ic_profile} />
           </View>
--- src/utils/Urlconfig.js
+++ src/utils/Urlconfig.js
@@ -3,6 +3,8 @@ const baseUrl = "http://localhost:3000";
 const API_URL = {
     LOGIN: baseUrl + "/users/login",
     REGISTER: baseUrl + "/users/register",
+    UPDATE_PROFILE: baseUrl + "/users/update-profile",
+    UPDATE_PASSWORD: baseUrl + "/users/update-password",
 }
 
 export {API_URL}

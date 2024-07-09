87a4e5c53e8a1026a6aff94b2474c8ec0670f7a8
naofalhakim <hakimnaofal@gmail.com>
2024-07-10 05:27:30 +0700

    chore: change gender setup code

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,6 +1,6 @@
 const getGenderText = (code) => {
-    if(code === 1) return 'Perempuan'
-    else return 'Laki-laki'
+    if(code === 1 || code === 0) return 'Laki-laki'
+    else return 'Perempuan'
 }
 
 export {getGenderText}

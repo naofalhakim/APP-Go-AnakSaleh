cd5802cee1e11e317121155a429fe4e9866f9974
naofalhakim <hakimnaofal@gmail.com>
2024-08-01 09:38:32 +0700

    feat: implement react-native-view-pager

--- src/screens/Materi/index.js
+++ src/screens/Materi/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Image, SafeAreaView, ScrollView, Text, TouchableOpacity, View } from 'react-native';
+import { Animated, Image, SafeAreaView, ScrollView, Text, TouchableOpacity, View } from 'react-native';
 import styles from './styles';
 import HeaderBasic from '../../components/HeaderBasic';
 import { SCREEN_NAME } from '../../utils/Enum';
@@ -8,6 +8,7 @@ import COLOR from '../../utils/ColorSystem';
 import { horizontalScale, moderateScale, verticalScale } from '../../utils/Metric';
 import Font from '../../assets/fonts';
 import ICON from '../../assets/icons';
+import PagerView from 'react-native-pager-view';
 
 const Materi = [
   {
@@ -17,21 +18,216 @@ const Materi = [
     unitFinished: 4,
     unitStatus: 3,
     thumbnail: "image"
+  },
+  {
+    title: "Muraqabah",
+    description: "Penjelasan singkat terkait silabus materi 2 / intro materi contoh: materi 2 akan mempelajari apa itu Muraqabah.",
+    unitTotal: 4,
+    unitFinished: 1,
+    unitStatus: 2,
+    thumbnail: "image"
   }
 ]
+
+const DOT_SIZE = 40;
+
+const Pagination = ({
+  scrollOffsetAnimatedValue,
+  positionAnimatedValue,
+  data
+}) => {
+  const inputRange = [0, data.length];
+  const translateX = Animated.add(
+    scrollOffsetAnimatedValue,
+    positionAnimatedValue
+  ).interpolate({
+    inputRange,
+    outputRange: [0, data.length * DOT_SIZE],
+  });
+
+  return (
+    <View style={[styles.pagination]}>
+      <Animated.View
+        style={[
+          styles.paginationIndicator,
+          {
+            position: 'absolute',
+            transform: [{ translateX: translateX }],
+          },
+        ]}
+      />
+      {data.map((item) => {
+        return (
+          <View key={item.key} style={styles.paginationDotContainer}>
+            <View
+              style={[styles.paginationDot, { backgroundColor: item.color }]}
+            />
+          </View>
+        );
+      })}
+    </View>
+  );
+};
 
 class MateriScreen extends Component {
   constructor(props) {
     super(props);
+
+    this._renderSwipeMenu = this._renderSwipeMenu.bind(this);
+    this._renderContainerMateri = this._renderContainerMateri.bind(this);
+    this.getUnitStatus = this.getUnitStatus.bind(this);
+
+    this.scrollOffsetAnimatedValue = null;
+    this.positionAnimatedValue = null;
+    // this.scrollOffsetAnimatedValue = React.useRef(new Animated.Value(0)).current;
+    // this.positionAnimatedValue = React.useRef(new Animated.Value(0)).current;
   }
 
-  getUnitStatus = (statusCode) => {
-    switch(statusCode){
+  getUnitStatus(statusCode) {
+    switch (statusCode) {
       case 1: return 'Belum dikerjakan';
       case 2: return "Sedang dipelajari"
       case 3: return "Selesai"
-      
+
     }
+  }
+  getButtonStatus = (statusCode) => {
+    switch (statusCode) {
+      case 1: return 'MULAI BELAJAR';
+      case 2: return "LANJUTKAN BELAJAR"
+      case 3: return "PELAJARI ULANG"
+
+    }
+  }
+
+  _renderSwipeMenu() {
+    return (
+      <PagerView initialPage={0}
+        pageMargin={horizontalScale(20)}
+        style={{
+          flex: 1,
+        }}
+        // onPageScroll={Animated.event(
+        //   [
+        //     {
+        //       nativeEvent: {
+        //         offset: this.scrollOffsetAnimatedValue,
+        //         position: this.positionAnimatedValue,
+        //       },
+        //     },
+        //   ],
+        //   {
+        //     listener: ({ nativeEvent: { offset, position } }) => {
+        //       console.log(`Position: ${position} Offset: ${offset}`);
+        //     },
+        //     useNativeDriver: true,
+        //   }
+        // )}
+        >
+        {Materi.map(this._renderContainerMateri)}
+      </PagerView>
+    )
+  }
+
+  _renderContainerMateri(data, index) {
+    const { illustration = ICON.ic_diamond, title = '', description = '', unitFinished = 0, unitTotal = 0, unitStatus = 0, } = data
+    return (
+      <View key={index} style={{
+        backgroundColor: COLOR.BLUE_EGG_DUCK,
+        borderRadius: 10,
+        padding: moderateScale(20),
+        shadowColor: COLOR.BLACK,
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.2,
+        shadowRadius: 3,
+        alignItems: 'center',
+      }}>
+        <View style={{
+          backgroundColor: COLOR.AQUARIUM_BLUE,
+          width: horizontalScale(186),
+          height: verticalScale(186),
+          borderRadius: 100,
+        }}>
+          <Image source={illustration} />
+        </View>
+
+        <View style={{
+          backgroundColor: COLOR.AQUARIUM_BLUE,
+          paddingHorizontal: horizontalScale(18),
+          paddingVertical: verticalScale(12),
+          borderRadius: moderateScale(10),
+          marginTop: verticalScale(15),
+          alignItems: 'center',
+        }}>
+          <Text style={{
+            color: COLOR.WHITE,
+            fontFamily: Font.NunitoBold,
+            fontSize: moderateScale(16),
+          }}>{'Materi ' + (index + 1) + ': ' + title}</Text>
+          <Text style={{
+            color: COLOR.WHITE,
+            fontFamily: Font.NunitoSemiBold,
+            fontSize: moderateScale(12),
+            marginTop: verticalScale(12),
+            marginBottom: verticalScale(14),
+            textAlign: 'center',
+          }}>{description}</Text>
+
+          <View style={{
+            flexDirection: 'row',
+            alignItems: 'center',
+          }}>
+            <Text style={{
+              color: COLOR.WHITE,
+              fontFamily: Font.NunitoBold,
+            }}>{unitFinished + " / " + unitTotal}</Text>
+
+            {unitStatus === 3 &&
+              <View style={{
+                backgroundColor: COLOR.WHITE,
+                height: verticalScale(16),
+                borderRadius: 10,
+                width: horizontalScale(2),
+                marginHorizontal: horizontalScale(12),
+              }} />
+            }
+
+            {unitStatus === 3 &&
+              <View style={{
+                flexDirection: 'row',
+                marginBottom: verticalScale(12),
+                marginTop: verticalScale(8)
+              }}>
+                <Image source={ICON.icon_status_unit} />
+                <Text style={{
+                  color: COLOR.WHITE,
+                  fontFamily: Font.NunitoSemiBold,
+                  marginLeft: horizontalScale(4),
+                }}>{this.getUnitStatus(unitStatus)}</Text>
+              </View>
+            }
+
+          </View>
+
+          <TouchableOpacity style={{
+            backgroundColor: COLOR.WHITE,
+            paddingVertical: verticalScale(12),
+            paddingHorizontal: moderateScale(18),
+            borderRadius: 20,
+            marginTop: 12,
+          }}>
+            <Text style={{
+              color: COLOR.CIYAN_Opaque,
+              fontFamily: Font.NunitoBold,
+              fontSize: 12
+            }}>
+              {this.getButtonStatus(unitStatus)}
+            </Text>
+          </TouchableOpacity>
+        </View>
+
+      </View>
+    )
   }
 
   render() {
@@ -43,93 +239,13 @@ class MateriScreen extends Component {
           rightComponent={() => <PointBar background={COLOR.TRANSPARENT} point={3000} />}
         />
         <View style={styles.margin}>
-          <View style={{
-            backgroundColor: COLOR.BLUE_EGG_DUCK,
-            borderRadius: 10,
-            padding: moderateScale(20),
-            shadowColor: COLOR.BLACK,
-            shadowOffset: {width: 0, height: 2},
-            shadowOpacity: 0.2,
-            shadowRadius: 3,
-            alignItems:'center',
-          }}>
-            <View style={{
-              backgroundColor: COLOR.AQUARIUM_BLUE,
-              width: horizontalScale(186),
-              height: verticalScale(186),
-              borderRadius: 100,
-            }}>
-              <Image />
-            </View>
-
-            <View style={{
-              backgroundColor: COLOR.AQUARIUM_BLUE,
-              paddingHorizontal: horizontalScale(18),
-              paddingVertical: verticalScale(12),
-              borderRadius: moderateScale(10),
-              marginTop: verticalScale(15),
-              alignItems:'center',
-            }}>
-              <Text style={{
-                color: COLOR.WHITE,
-                fontFamily: Font.NunitoBold,
-                fontSize: moderateScale(16),
-              }}>Materi 1: {Materi[0].title}</Text>
-              <Text style={{
-                color: COLOR.WHITE,
-                fontFamily: Font.NunitoSemiBold,
-                fontSize: moderateScale(12),
-                marginTop: verticalScale(12),
-                marginBottom: verticalScale(14),
-                textAlign: 'center',
-              }}>{Materi[0].description}</Text>
-              <View style={{
-                flexDirection:'row',
-                alignItems:'center',
-                justifyContent: 'center'
-              }}>
-                <Text style={{
-                  color: COLOR.WHITE,
-                  fontFamily: Font.NunitoBold,
-                }}>{Materi[0].unitFinished + " / " + Materi[0].unitTotal}</Text>
-                <View style={{
-                  backgroundColor: COLOR.WHITE,
-                  height: '70%',
-                  borderRadius: 10,
-                  width: horizontalScale(2),
-                  marginHorizontal: horizontalScale(12),
-                }} />
-                <View style={{
-                  flexDirection: 'row',
-                  alignItems: 'center',
-                  marginBottom: verticalScale(12)
-                }}>
-                  <Image source={ICON.icon_status_unit}/>
-                  <Text style={{
-                  color: COLOR.WHITE,
-                  fontFamily: Font.NunitoSemiBold,
-                  marginLeft: horizontalScale(4),
-                }}>{getUnitStatus(Materi[0].unitStatus)}</Text>
-                </View>
-              </View>
-              <TouchableOpacity style={{
-                backgroundColor: COLOR.WHITE,
-                paddingVertical: verticalScale(12),
-                paddingHorizontal: moderateScale(18),
-                borderRadius:20,
-                marginTop: 12,
-              }}>
-                <Text style={{
-                  color: COLOR.CIYAN_Opaque,
-                  fontFamily: Font.NunitoBold,
-                  fontSize:12
-                }}>
-                Pelajari Ulang
-                </Text>
-                </TouchableOpacity>
-            </View>
-
-          </View>
+          {this._renderSwipeMenu()}
+          
+          {/* <Pagination
+            data={Materi}
+            scrollOffsetAnimatedValue={this.scrollOffsetAnimatedValue}
+            positionAnimatedValue={this.positionAnimatedValue}
+          /> */}
         </View>
       </SafeAreaView>
     )
--- src/screens/Materi/styles.js
+++ src/screens/Materi/styles.js
@@ -3,6 +3,8 @@ import { horizontalScale, moderateScale, verticalScale } from "../../utils/Metri
 import COLOR from "../../utils/ColorSystem";
 import Font from "../../assets/fonts";
 
+const DOT_SIZE = 40;
+
 const styles = StyleSheet.create({
     container: {
         flex: 1,
@@ -10,97 +12,36 @@ const styles = StyleSheet.create({
     margin: { 
         padding: moderateScale(22),
         flex: 1,
-    },
-    photoContainer: {
-        width: horizontalScale(130),
-        height: verticalScale(130),
-        borderWidth: 1,
-        borderColor: COLOR.GREEN_WHITE,
-        borderRadius: 100,
-        alignItems: 'center',
-        justifyContent: 'center',
-        alignSelf: 'center'
-    },
-    nameText:{
-        marginTop: 18,
-        fontFamily: Font.NunitoSemiBold,
-        fontSize:20,
-        color: COLOR.BLUE_WHALE,
-        alignSelf:'center',
-    },
-    emailText:{
-        fontFamily: Font.NunitoSemiBold,
-        fontSize:16,
-        color: COLOR.BLUISH_GREY,
-        alignSelf:'center',
-    },
-    captionPointText:{
-        fontFamily: Font.NunitoBold,
-        fontSize: 14,
-        color: COLOR.BLUISH_GREY,
-        maxWidth:'40%'
-    },
-    pointContainer:{
-        backgroundColor: COLOR.BLUE_EGG_DUCK,
-        padding: 10,
-        borderRadius: 10,
-        flexDirection: 'row'
-    },
-    imageDiamond:{
-        width:horizontalScale(15),
-        height:verticalScale(22),
-        marginRight: horizontalScale(6),
-    },  
+    }, 
     pointText:{
         fontFamily: Font.NunitoBlack,
         fontSize: 18,
         color: COLOR.BLACK,
     },
-    pointSectionContainer:{
-        marginTop: verticalScale(8), 
-        borderWidth: 2,
-        borderRadius: 10,
-        padding: 12,
-        borderColor: COLOR.BLUE_EGG_DUCK,
-    },
-    pointSection:{ 
-        flexDirection: 'row', 
-        justifyContent: 'space-between',
-        marginBottom: 12
-    },
-    textLinkReward:{
-        alignSelf:'center',
-        color: COLOR.BLUE_PRIMER,
-        fontFamily: Font.NunitoBold,
-        fontSize: 12,
-        textDecorationLine: 'underline',
-      },
-      userSection: {
-        backgroundColor: COLOR.BLUE_EGG_DUCK,
-        padding: moderateScale(12),
-        marginVertical: verticalScale(12),
-        borderRadius: 10,
-      },
-      iconEdit: {
-        width:horizontalScale(18), 
-        height: verticalScale(18),
-        alignSelf: 'flex-end',
-    },
-    dataSection: {
-        flexDirection:'row',
-        justifyContent:'space-between',
-        marginVertical: verticalScale(6),
-    },
-    textLabel:{
-        color: COLOR.BLUE_WHALE,
-        fontFamily: Font.NunitoBold,
-        fontSize: moderateScale(16),
-    },
-    valueLabel:{
-        color: COLOR.BLUISH_GREY,
-        fontFamily: Font.NunitoSemiBold,
-        fontSize: moderateScale(14),
-    }
+    pagination: {
+    position: 'absolute',
+    right: 20,
+    bottom: 40,
+    flexDirection: 'row',
+    height: DOT_SIZE,
+  },
+  paginationDot: {
+    width: DOT_SIZE * 0.3,
+    height: DOT_SIZE * 0.3,
+    borderRadius: DOT_SIZE * 0.15,
+  },
+  paginationDotContainer: {
+    width: DOT_SIZE,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  paginationIndicator: {
+    width: DOT_SIZE,
+    height: DOT_SIZE,
+    borderRadius: DOT_SIZE / 2,
+    borderWidth: 2,
+    borderColor: '#ddd',
+  },
 });
 
 export default styles;
